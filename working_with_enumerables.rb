95087c527d80d9c515fc2fc6138e1de9c7591c47
Torey Hickman <torey@toreyhickman.com>
2015-08-19 12:54:27 -0500

    Edit comment in file

--- working_with_enumerables.rb
+++ working_with_enumerables.rb
@@ -1,32 +1,29 @@
-#Test your methods by running the spec as you work on each method
-#You'll probably run the spec a lot! Run it now to see that all tests are failing.
-
-# TODO: Return array of elements at indices 1, 3, 5, 7, etc.
-#       You should make use of Enumerable#each_with_index
+# TODO: Returns an array of elements at indices 1, 3, 5, 7, etc.
+#       Use #each_with_index.
 def odd_indexed_elements(array)
 end
 
 # TODO: Return the odd numbers from a list of integers.
-#       You should make use of Enumerable#select
+#       Use #select.
 def odd_integers(array)
 end
 
 # TODO: Return the first number from an Array that is less than a particular number - 'limit.'
-#       You should make use of Enumerable#find
+#       Use #find.
 def first_under(array, limit)
 end
 
 # TODO: Take an Array of Strings and return a new Array with an exclamation point appended to each String.
-#       You should make use of Enumerable#map
+#       Use #map.
 def add_bang(array)
 end
 
 # TODO: Calculate the sum of an Array of numbers.
-#       You should make use of Enumerable#reduce
+#       Use #reduce.
 def sum(array)
 end
 
 # TODO: Reorganize an Array of the elements into groups of 3, and then sort each group alphabetically.
-#       You should make use of Enumerable#each_slice
+#       Use #each_slice in combination with other enumerable methods.
 def sorted_triples(array)
 end
